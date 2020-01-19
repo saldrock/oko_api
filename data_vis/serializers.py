d88978c0f23f3b38b243079caaf2f5f9e7a6ca7f
Bernard <hollandsbernard@gmail.com>
2020-01-19 15:50:09 +0000

    created user serializer

--- data_vis/serializers.py
+++ data_vis/serializers.py
@@ -6,3 +6,16 @@ from django.contrib.auth.models import User
 class RoomSerializer(serializers.ModelSerializer):
     model = Room
     fields = ('id', 'name', 'period')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password':{'write_only': True, 'required':True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
+
