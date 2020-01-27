891831fe41a075443780d94e88aa9d73f4c36a63
Bernard <hollandsbernard@gmail.com>
2020-01-27 13:05:00 +0000

    created inital model structures

--- house/models.py
+++ house/models.py
@@ -1,9 +1,25 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Room(models.Model): #Our room model
+class house(models.Model)
+    no_of_rooms = models.IntegerField()
 
-class Data_Report(models.Model):#data reporting model
+    #all calulated
+    def avgTemp(self):
+    def avgHum(self):
+    def avgCo2(self):
+
+class Room(models.Model): #Our room model
+    name = models.CharField(max_length=36)  # name of the room
+
+    #all calcuated
+    def humidity(self):
+    def tempurature(self):
+    def Co2(self):
+
+
+class DataReport(models.Model):#data reporting model
+    def lifetime(self):
+
 
 class Control(models.Model): #device control model (mabye handled by react???)
-
