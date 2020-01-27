d134bb273671dfda8b94b121ddc7d5e7f3755fbb
Bernard <hollandsbernard@gmail.com>
2020-01-27 12:53:25 +0000

    created base models

--- house/models.py
+++ house/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Room(models.Model): #Our room model
+
+class Data_Report(models.Model):#data reporting model
+
+class Control(models.Model): #device control model (mabye handled by react???)
+
