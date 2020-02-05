71a31cfd4a050bb51db0571afe750bd60843fea5
Bernard <hollandsbernard@gmail.com>
2020-02-05 22:51:25 +0000

    newest version with comments From: Chicken

--- house/models.py
+++ house/models.py
@@ -2,22 +2,28 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+# User stuff Neve
 class Room(models.Model):
     name = models.CharField(max_length=36)  # name of the room
 
 
 class House(models.Model):
     name = models.CharField(max_length=36)  # name of the room
+    rooms = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='rooms', null=True)
 
 
-class Control(models.Model):
-    name = models.CharField(max_length=36)  # name of the room
-
-
-class Suggestion(models.Model):
-    name = models.CharField(max_length=36)  # name of the room
-
-
-class Devices(models.Model):
+class Devices(models.Model):  # Bernie
     name = models.CharField(max_length=36)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='devices', null=True)
+
+
+class Control(models.Model):  # Probably not us, controllng smart devices
+    name = models.CharField(max_length=36)  # name of the room
+
+
+class Suggestion(models.Model):  # Harvie
+    name = models.CharField(max_length=36)  # name of the room
+
+
+class Readings(models.Model):  # Neve, from the house
+    name = models.CharField(max_length=36)
