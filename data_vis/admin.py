15ab90d60b8e7b5a448a61917e3d7162148bcc85
Bernard <hollandsbernard@gmail.com>
2020-02-25 14:46:53 +0000

    Merge branch 'master' of /home/bernard/Projects/oko_api with conflicts.

--- data_vis/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Room, TimePeriod, DataType
-
-admin.site.register(Room)
-admin.site.register(TimePeriod)
-admin.site.register(DataType)
-
--- house/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Room, House, Control, Suggestion, Devices
-
-admin.site.register(Room)
-admin.site.register(House)
-admin.site.register(Control)
-admin.site.register(Suggestion)
-admin.site.register(Devices)
