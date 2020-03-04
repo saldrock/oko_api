995bd9d3b7202cf0a794f773f682270c8006dcac
ncurnyn <ncurnyn@yahoo.co.uk>
2020-03-04 17:09:01 +0000

    fixed related names

--- house/models.py
+++ house/models.py
@@ -26,7 +26,7 @@ class Suggestions(models.Model):
 
 class Readings(models.Model):  
     reading_id = models.IntegerField(primary_key=True) #ID number of suggestion
-    device_id = models.ForeignKey(Devices, on_delete=models.CASCADE, related_name=device_id) #device that the reading is from
+    device_id = models.ForeignKey(Devices, on_delete=models.CASCADE, related_name='device_id') #device that the reading is from
     reading_type = models.CharField(max_length=50,null=False) #type of reading eg co2, humidity, heating etc
     reading = models.IntegerField(null=False) # the actual reading
 
@@ -36,14 +36,14 @@ class Login(models.Model):
 
 class User(models.Model):
     email = models.CharField(primary_key=True, max_length=100, default='')  
-    username = models.ForeignKey(Login, on_delete=models.CASCADE, related_name=Login, max_length=60, null=False) 
+    username = models.ForeignKey(Login, on_delete=models.CASCADE, related_name='Login', max_length=60, null=False) 
     first_name = models.CharField(max_length=35,default='')  
     surname = models.CharField(max_length=35, default='')  
-    dwelling_id = models.ForeignKey(Dwellings, on_delete=models.CASCADE, related_name=dwelling_id)
+    dwelling_id = models.ForeignKey(Dwellings, on_delete=models.CASCADE, related_name='dwelling_id')
     phone_number = models.CharField(max_length=13,default='')
     incentivisation_choice = models.CharField(max_length=13, default='')
 
 class Leaderboard(models.Model):
-    username = models.ForeignKey(Login, on_delete=models.CASCADE, related_name= username)
+    username = models.ForeignKey(Login, on_delete=models.CASCADE, related_name= 'username')
     points = models.IntegerField(blank=True)
     ranking = models.IntegerField(blanK=False)
