92b64582f59b02131828c42e531191ad832b9a9d
Bernard <hollandsbernard@gmail.com>
2020-03-24 17:51:39 +0000

    Auth Token generation added

--- Dwelling/models.py
+++ Dwelling/models.py
@@ -13,11 +13,17 @@ class Dwelling(models.Model):
     dwelling_members = models.CharField(max_length=50000, null=False, default='')  # dwelling code used to sign up to dwelling
     dwelling_superUsers = models.CharField(max_length=50000, null=False, default='')  # dwelling code used to sign up to dwelling
 
+    def __str__(self):
+        return self.dwelling_name
+
 
 class Room(models.Model):
     room_id = models.IntegerField(primary_key=True, null=False, default=0)  # ID number of room
     room_name = models.CharField(max_length=50, null=False, default='')  # name of the room
     dwelling_rooms = models.ForeignKey(Dwelling, on_delete=models.CASCADE, related_name='rooms')
+
+    def __str__(self):
+        return self.room_name
 
 
 class RoomData(models.Model):
@@ -28,13 +34,17 @@ class RoomData(models.Model):
     rooms_data = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='room_data', default='')
 
 
+
 class Device(models.Model):
     device_name = models.CharField(max_length=50, null=False, default='')  # name of device
     state = models.BooleanField(default=False),  # if the device is on or off
     mac_adress = models.CharField(max_length=50, null=False, default='')  # name of device
     room_devices = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='devices')
 
+
+
 class Suggestion(models.Model):
     suggestion = models.CharField(max_length=150, null=False, default=0)  # suggestion on what to do
     room_suggestion = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='suggestion')
+
 
--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from .serializers import RegistrationSerializer
+from rest_framework.authtoken.models import Token
 
 
 @api_view(['POST', ])
@@ -16,6 +17,8 @@ def registration_view(request):
 			data['response'] = 'successfully registered new user.'
 			data['email'] = account.email
 			data['username'] = account.username
+			token = Token.objects.get(user=account).key
+			data['token'] = token
 		else:
 			data = serializer.errors
 		return Response(data)
